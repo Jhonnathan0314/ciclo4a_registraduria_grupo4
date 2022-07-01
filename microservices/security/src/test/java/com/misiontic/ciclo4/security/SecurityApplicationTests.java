e769ec5aef81810f28a661bd22249004d8528ca7
Orlando Vargas <nojipiz@protonmail.com>
2022-06-30 21:42:18 -0500

    Add spring initializer trash boilerplate :v

---
+++ microservices/security/src/test/java/com/misiontic/ciclo4/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.ciclo4.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
