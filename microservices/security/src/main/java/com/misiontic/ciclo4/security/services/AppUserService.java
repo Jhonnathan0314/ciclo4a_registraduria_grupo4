fb1cf57955e4b2902d572f6b6f0f7e262da8ccfd
Orlando Vargas <nojipiz@protonmail.com>
2022-07-02 20:50:06 -0500

    Added simple enpoint for test

--- microservices/security/src/main/java/com/misiontic/ciclo4/security/services/AppUserService.java
+++ microservices/security/src/main/java/com/misiontic/ciclo4/security/services/AppUserService.java
@@ -3,6 +3,7 @@ package com.misiontic.ciclo4.security.services;
 import com.misiontic.ciclo4.security.models.AppUser;
 
 public interface AppUserService {
+
   AppUser findUserById(String userId);
 
   void addUser(AppUser user);
--- microservices/security/src/main/java/com/misiontic/ciclo4/security/services/AppUserServiceImpl.java
+++ microservices/security/src/main/java/com/misiontic/ciclo4/security/services/AppUserServiceImpl.java
@@ -1,7 +1,16 @@
 package com.misiontic.ciclo4.security.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +19,12 @@ import com.misiontic.ciclo4.security.repositories.AppUserRepository;
 
 @Service
 @Transactional
-public class AppUserServiceImpl implements AppUserService {
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
 
   @Autowired
   private AppUserRepository userRepo;
   @Autowired
-  private PasswordEncoder passwordEncoder;
+  private BCryptPasswordEncoder passwordEncoder;
 
   @Override
   public AppUser findUserById(String userId) {
@@ -32,5 +41,15 @@ public class AppUserServiceImpl implements AppUserService {
         encodedPassword,
         user.roles());
     userRepo.save(userWithEncodedPassword);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    final List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+    authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+    final AppUser user = userRepo.findByUserName(username);
+    if (user == null)
+      throw new UsernameNotFoundException("User not found");
+    return new User(user.username(), user.password(), authorities);
   }
 }
