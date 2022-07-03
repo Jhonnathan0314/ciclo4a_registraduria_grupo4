b8da2e3ad7fab50924e4dbe1f956501e5fd4b478
Orlando Vargas <nojipiz@protonmail.com>
2022-07-02 20:49:44 -0500

    Add boilerplate of JWT for acounts

---
+++ microservices/security/src/main/java/com/misiontic/ciclo4/security/filters/CustomAuthFilter.java
@@ -0,0 +1,62 @@
+package com.misiontic.ciclo4.security.filters;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class CustomAuthFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final AuthenticationManager authenticationManager;
+
+  public CustomAuthFilter(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
+  }
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+      throws AuthenticationException {
+    final String username = request.getParameter("username");
+    final String password = request.getParameter("password");
+    final var authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+    return authenticationManager.authenticate(authenticationToken);
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+      Authentication authResult) throws IOException, ServletException {
+    final var user = (User) authResult.getPrincipal();
+    final var algorithm = Algorithm.HMAC256("secret".getBytes());
+    final String accessToken = JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+        .withIssuer(request.getRequestURL().toString())
+        .withClaim("roles",
+            user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+        .sign(algorithm);
+    final String refreshToken = JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + 600 * 60 * 1000))
+        .withIssuer(request.getRequestURL().toString())
+        .withClaim("roles",
+            user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+        .sign(algorithm);
+    response.setHeader("access_token", accessToken);
+    response.setHeader("refresh_token", refreshToken);
+  }
+}
