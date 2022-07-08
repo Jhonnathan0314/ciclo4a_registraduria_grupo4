a83fe535c51588bc0217095d3edbf1f7a812844b
Orlando Vargas <nojipiz@protonmail.com>
2022-07-08 03:35:46 -0500

    Removed dependency injection useless runner

--- microservices/security/src/main/java/com/misiontic/ciclo4/security/SecurityApplication.java
+++ microservices/security/src/main/java/com/misiontic/ciclo4/security/SecurityApplication.java
@@ -1,34 +1,12 @@
 package com.misiontic.ciclo4.security;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.misiontic.ciclo4.security.models.AppUser;
-import com.misiontic.ciclo4.security.services.AppUserServiceImpl;
 
 @SpringBootApplication
 public class SecurityApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SecurityApplication.class, args);
-  }
-
-  @Bean
-  CommandLineRunner runner(AppUserServiceImpl service) {
-    Collection<String> sampleRoles = new ArrayList<>();
-    sampleRoles.add("admin");
-    sampleRoles.add("user");
-    return args -> {
-      service.addUser(
-          new AppUser("001", "userOne", "email@email.com", "pass",
-              sampleRoles));
-      service.addUser(new AppUser("002", "userTwo", "emailqemail.com",
-          "pass", sampleRoles));
-    };
   }
 }
