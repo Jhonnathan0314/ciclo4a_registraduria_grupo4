f103f19662388e801e66934b89ab771cbd75f8ba
Orlando Vargas <nojipiz@protonmail.com>
2022-07-08 05:07:47 -0500

    Fixed return type

--- microservices/security/src/main/java/com/misiontic/ciclo4/security/models/Role.java
+++ microservices/security/src/main/java/com/misiontic/ciclo4/security/models/Role.java
@@ -1,7 +1,10 @@
 package com.misiontic.ciclo4.security.models;
 
-/**
- * Role
- */
-public record Role(String id, String name) {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Role")
+public record Role(
+    @Id String id,
+    String name) {
 }
